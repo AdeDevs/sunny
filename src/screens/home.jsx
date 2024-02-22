50dd67b50ac5af5316c5b1ccab373c5444b6adaa
AdeDevs <adeyemiakinyemi01@gmail.com>
2024-02-22 16:06:05 +0100

    vercel

--- src/screens/home.jsx
+++ src/screens/home.jsx
@@ -1,7 +1,32 @@
+import Logo from  '../assets/logo.png'
+import Menu from '../assets/hamburger.png'
+import Arrow from "../assets/arrowdown.png"
+import Egg from "../assets/desktop/egg.jpg"
+import Standout from "../assets/desktop/standout.jpg"
+import EggMob from "../assets/mobile/egg.jpg"
+import StandoutMob from "../assets/mobile/standout.jpg"
+
 function HomeScreen() {
     return (
         <div className="home-box">
-            <h1>Sunny Side</h1>
+            <header>
+                <nav>
+                    <img src={Logo} alt="logo" />
+                    <img id='menu' src={Menu} alt="menu" />
+                    <ul>
+                        <li>About</li>
+                        <li>Services</li>
+                        <li>Project</li>
+                        <button>Contact</button>
+                    </ul>
+                </nav>
+                <div>
+                    <h1>We are creatives</h1>
+                    <img src={Arrow} alt="arrow" />
+                </div>
+            </header>
+            <main>
+            </main>
         </div>
     )
 }
